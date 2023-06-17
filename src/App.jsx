29a0068f7af4ad4b9535678c63ee1b5eab420e2b
Kathlyn <kathlyn714@gmail.com>
2023-06-17 01:59:47 -0300

    add import global css and componets Header, Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-function App() {
+import { Post } from "./components/Post"
+import { Header } from "./components/Header"
+
+import './global.css';
+
+export function App() {
 
   return (
-    <h1>Hellow World</h1>
+    <>
+      <Header />
+      <Post author="Naruto Uzumaki" content="Sasukeeeeeeeeeeeeee" />
+      <Post author="Sasuke Uchira" content="Narutoooooooooooooo" />
+    </>
   )
 }
 
-export default App
