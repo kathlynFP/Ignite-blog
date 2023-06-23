dcb2e056e6e2a6e9f808b18fe07d9a5d15a1724d
Kathlyn <kathlyn714@gmail.com>
2023-06-23 19:06:51 -0300

    add components Sidebar and alters Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Post } from "./components/Post"
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar";
+
+import styles from './App.module.css'
 
 import './global.css';
 
@@ -8,8 +11,14 @@ export function App() {
   return (
     <>
       <Header />
-      <Post author="Naruto Uzumaki" content="Sasukeeeeeeeeeeeeee" />
-      <Post author="Sasuke Uchira" content="Narutoooooooooooooo" />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Naruto Uzumaki" content="Sasukeeeeeeeeeeeeee" />
+          <Post author="Naruto Uzumaki" content="Narutoooooooooooooo" />          
+        </main>
+      </div>
     </>
   )
 }
