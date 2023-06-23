927a230cad1cb9743a6d9c0cfed282a5595477a5
Kathlyn <kathlyn714@gmail.com>
2023-06-23 18:57:52 -0300

    create component Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import style from './Header.module.css';
 
+import logoIgnite from '../assets/logo-ignite.svg'
+
 export const Header = () => {
+
+	console.log(logoIgnite);
 
 	return (
 		<header className={style.header}>
+			<img src={logoIgnite} alt='Logo Ignite' />
 			<strong >Ignite Feed</strong>
 		</header>
 	);
